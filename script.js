34b86fb8f422431cdbea61750d4c1c2fe2a705fa
Francesco Puppo <fpuppo@spotzer.com>
2018-04-18 18:14:42 +0200

    fix

--- script.js
+++ script.js
@@ -2,15 +2,10 @@ $(document).ready(function () {
     $(".submit").click(function () {
         var dudaid = $("#dudaid").val();
         var selected = $('#device').children(":selected").val();
-        
-console.log($('input[name="iamdesktop"]').is(':checked'));
-
-        if( $('input[name="iamdesktop"]').is(':checked'))
-        {
-            $("#form").attr('action', 'https://my.duda.co/preview/' + dudaid);   
-        }
-        else {
-            $("#form").attr('action', 'https://my.duda.co/site/' + dudaid + '?showOriginal=true&preview=true&insitepreview=true&dm_device=' + selected);
+        $("#form").attr('action', 'https://my.duda.co/site/' + dudaid + '?showOriginal=true&preview=true&insitepreview=true&dm_device=' + selected);
+        if ($('input[name="iamdesktop"]').is(':checked')) {
+            $('#form').attr("method", "get");
+            $("#form").attr('action', 'https://my.duda.co/preview/' + dudaid);
         }
     });
 });
