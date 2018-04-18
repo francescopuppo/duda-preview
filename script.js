bae9b8be2d2ce1219d3d6697ce54c7a0ab015b12
Francesco Puppo <fpuppo@spotzer.com>
2018-04-18 17:19:50 +0200

    HTML changes

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ $(document).ready(function () {
         var dudaid = $("#dudaid").val();
         var selected = $('#device').children(":selected").val();
         $("#form").attr('action', 'https://my.duda.co/site/' + dudaid + '?showOriginal=true&preview=true&insitepreview=true&dm_device=' + selected);
+        if 
+        
     });
 });
