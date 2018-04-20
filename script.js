dec7a5d283f2c9f1cc1df1b0b8b9f28a7cd645af
Francesco Puppo <fpuppo@spotzer.com>
2018-04-20 12:32:14 +0200

    JS fix

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
     $(".submit").click(function () {
         var dudaid = $("#dudaid").val();
         var selected = $('#device').children(":selected").val();
-        $("#form").attr('action', 'https://my.duda.co/preview/' + dudaid + '?showOriginal=true&preview=true&insitepreview=true&dm_device=' + selected);
         if ($('input[name="iamdesktop"]').is(':checked')) {
-            $('#form').attr("method", "get");
-            $("#form").attr('action', 'https://my.duda.co/site/' + dudaid);
+            $("#form").attr('action', 'https://my.duda.co/preview/' + dudaid);
+            $("#form").attr('method', 'get'); 
         }
+        else {$("#form").attr('action', 'https://my.duda.co/site/' + dudaid + '?showOriginal=true&preview=true&insitepreview=true&dm_device=' + selected);}
     });
 });
 
